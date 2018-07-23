4312d9acda5d6c43e57fd7e9b73e37aa9a6edfc7
mati4ever <mateusz.zasko7@gmail.com>
2018-07-23 11:58:34 +0200

    Overloading, params

---
+++ Members/Diary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Members
+{
+    public class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+            count ++;
+        }
+
+        List<float> ratings;
+
+        public static float minGrade = 0;
+        public static float maxGrade = 10;
+        public static long count = 0;
+
+        public string Name;
+
+        public DiaryStatistics ComputeStatistics()
+        {
+            DiaryStatistics stats = new DiaryStatistics();
+
+            float sum = 0f;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            stats.averageGrade = sum / ratings.Count();
+            stats.maxGrade = ratings.Max();
+            stats.minGrade = ratings.Min();
+
+
+            return stats;
+        }
+
+
+        /// <summary>
+        ///     Adding rating to diary.
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddRating(float rating)
+        {
+            if (rating >= 0 && rating <= 10)
+            {
+                ratings.Add(rating);
+            }
+        }
+
+        ///// <summary>
+        /////  Calculating the average of our rating.
+        ///// </summary>
+        ///// <returns></returns>
+        //public float CalculateAverage()
+        //{
+        //    float sum = 0, avg = 0;
+
+        //    foreach (var rating in ratings)
+        //    {
+
+        //        sum += rating;
+        //    }
+
+        //    avg = sum / ratings.Count();
+        //    return avg;
+
+        //}
+
+        //public float GiveMaxRating()
+        //{
+        //    return ratings.Max();
+        //}
+
+        //public float GiveMinRating()
+        //{
+        //    return ratings.Min();
+
+        //}
+
+
+
+    }
+}
---
+++ Members/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Diary diary = new Diary();
+            diary.AddRating(8.5f);
+            diary.AddRating(5f);
+            diary.AddRating(3.5f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+
+            WriteResult("Average", stats.averageGrade, 5,7,8);
+            WriteResult("Highest rating", (int)stats.maxGrade);
+            WriteResult("Lowest rating" , (long)stats.minGrade);
+            WriteResult("TEST", stats.minGrade, 1);
+
+            Console.ReadKey();
+
+        }
+
+        private static void WriteResult(string description, params float[] result)
+        { 
+            Console.WriteLine(description + ": " + result[0] + result[1] + result[2]);
+        }
+
+        private static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        private static void WriteResult(string description, long result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        private static void WriteResult(string description, float result, long test)
+        {
+            // Dwa równoważne zapisy - znak $ pozwala na bezpośrednie zapisanie w place holderze parametru.
+            Console.WriteLine("{0} {1:F2} {2:C} {3:C1} {4} {5}", description, result , 2,3,4,5);
+            Console.WriteLine($"{description} {result:F2} {2:C} {3:C1} {4} {5}"); 
+         
+        }
+
+    }
+}
